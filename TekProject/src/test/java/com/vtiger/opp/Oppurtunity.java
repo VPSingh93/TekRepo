320829af1b307f6344a85550c805d94d44693f22
VPSingh93 <viswa12345678@gmail.com>
2025-07-18 17:20:37 +0530

    Hi am committed.

---
+++ TekProject/src/test/java/com/vtiger/opp/Oppurtunity.java
@@ -0,0 +1,12 @@
+package com.vtiger.opp;
+
+import org.testng.annotations.Test;
+
+public class Oppurtunity 
+{
+	@Test
+	public void createOpp()
+	{
+		System.out.println("Oppurtunity created successfully");
+	}
+}
