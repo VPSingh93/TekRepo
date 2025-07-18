883963f7b42f8003787eeb20ed6a1be7de2b49ce
VPSingh93 <viswa12345678@gmail.com>
2025-07-18 16:44:53 +0530

    commitViswa

---
+++ TekProject/src/test/java/com/vtiger/contact/CreateContactTest.java
@@ -0,0 +1,17 @@
+package com.vtiger.contact;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateContactTest
+{
+	@Test
+	public void createContact()
+	{
+		Reporter.log("Contact created successfully",true);
+	}
+	
+	
+	
+	
+}
---
+++ TekProject/src/test/java/com/vtiger/organisation/CreateOrgTest.java
@@ -0,0 +1,13 @@
+package com.vtiger.organisation;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateOrgTest 
+{
+	@Test
+	public void createOrganisation()
+	{
+		Reporter.log("Organisation created successfully",true);
+	}
+}
